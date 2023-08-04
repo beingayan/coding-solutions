6a3810b73b3b3967c5db9d7de1fb4c56a2685382
beingayan <ayankhatibbca@gmail.com>
2023-08-04 11:45:14 +0530

    email validation added

--- validation.js
+++ validation.js
@@ -1,13 +1,41 @@
-<script>
-     {/* #number validation  */}
-    {/* accept only numbers  start */}
+
+/*
+  * * * * * * * * * * * * * * * * * * * * * * 
+  *                                         *
+  *         #number validation  
+  *            accept only numbers          * 
+  *  * * * * * * * * * * * * * * * * * * * * *                                          
+*/
+ 
   const onKeyDown=
-  {(e) => {
+  (e) => {
     if (e.key === "." || (!/^[0-9\b]+$/.test(e.key) && e.key !== "Backspace")) {
       e.preventDefault();
     }
-  }}
+  }
   <input type="text" onKeyDown={onkeydown} />
 
-  {/* accept only numbers  end */}
-</script>;
+ 
+
+
+/*
+  * * * * * * * * * * * * * * * * * * * * * * 
+  *                                         *
+  *         #email validation                                *
+  *  * * * * * * * * * * * * * * * * * * * * *                                          
+*/
+
+
+
+  function isValidEmail(email) {
+  // Regular expression pattern for email validation
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+  return emailPattern.test(email);
+}
+
+// Test the function
+console.log(isValidEmail("test@example.com")); // true
+console.log(isValidEmail("invalid_email")); // false
+
+
